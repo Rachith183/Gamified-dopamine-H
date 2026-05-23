3a4278897463b7ea592f4c1eaf07138c20c52b7a
Rachith <rachith@local>
2026-05-23 10:38:59 +0530

    Simplify: Run Express server directly on Vercel instead of using Functions

--- api/index.js
+++
@@ -1,6 +0,0 @@
-// Vercel serverless function handler
-import app from '../backend/server.js';
-
-// Vercel expects either a default export that's an Express app or a handler function
-// Express apps can be directly exported as the handler
-export default app;
